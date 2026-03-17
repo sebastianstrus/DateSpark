68c7e981f4a557a98b4c004f9fdee549da3f84b2
Sebastian Strus <sebastian.strus1@gmail.com>
2026-03-17 19:59:13 +0100

    Initial Commit

---
+++ DateSpark/DateSparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateSparkApp.swift
+//  DateSpark
+//
+//  Created by Sebastian Strus on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct DateSparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
